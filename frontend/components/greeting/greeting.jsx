5948d28f0679de284d5f640b62e195ad488f5d6e
Justin Cheasty <cheasty@gmail.com>
2021-04-10 00:34:20 -0700

    update user signin page, render images and video on homepage

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,18 +5,65 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
-    <div className="header">
-      
-      <Link to="/" className="header-link">
-        <h3>🦍 Robinherd 🦍</h3>
-      </Link>
-    <nav className="login-signup">
-      <ul>
-      <li className="login-button"><Link to="/login">Log In</Link></li>
-       <Link to="/signup"><li className="signup-button">Sign Up</li></Link>
-      </ul>
-      
-    </nav>
+    <div>
+
+      <div className="green-part">
+        <div className="header">
+          <Link to="/" className="header-link">
+            <h3>🦍 Robinherd 🦍</h3>
+          </Link>
+          <nav className="login-signup">
+            <ul>
+            <li className="login-button"><Link to="/login">Log In</Link></li>
+              <Link to="/signup"><li className="signup-button">Sign Up</li></Link>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="green-for-real">
+          
+          <div className="green-left">
+            <div><h1>Investing for errbody!</h1></div>
+            <div><h2>Commission-free investing, plus the tools you need to put your money in motion. Sign up and get your first stock for free. Probably something wack like HECLA.</h2></div>
+            <div><Link to="/signup"><span className="signup-button-deux"><h3>Sign Up</h3></span></Link></div>
+            <div className="icon-info">
+              <span><svg fill="none" height="28" viewBox="0 0 24 24" width="28"><circle cx="12" cy="12" r="11" stroke="rgb(0, 0, 0)" strokeWidth="2"></circle><path d="M11.232 18H13.056V9.52H11.232V18ZM11.2 8.128H13.088V6.32H11.2V8.128Z" fill="rgb(0, 0, 0)"></path></svg></span>
+              <div className="icon-info-text">Commissions {'&'} Free Stonk Disclosures</div>
+            </div>
+          </div>
+
+          <div className="green-right">
+            <video
+              className="phone-vid" 
+              autoPlay 
+              controlsList="nodownload nofullscreen noremoteplayback" 
+              loop
+              muted
+              playsInline
+              preload="auto" 
+            >
+              <source src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__327bf4cc768a323497d5aaa7416319c2.mp4" type="video/mp4"/>
+            </video>
+
+              <img class="phone-img" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png" srcSet="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/2x__ff9c36e27d7018cf707b95d8675793a3.png 2x, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__45f00d7b296cb52968f1bca4ef766fc1.png 3x"></img>
+          </div>
+
+        </div>
+      </div>
+
+      <div>
+        <video 
+              autoPlay 
+              controlsList="nodownload nofullscreen noremoteplayback" 
+              loop
+              muted
+              playsInline
+              preload="auto" 
+            >
+              <source src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4" type="video/mp4"/>
+            </video>
+      </div>
+
     </div>
   );
   const personalGreeting = () => (
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -41,59 +41,40 @@ class SessionForm extends React.Component {
     );
   }
 
+  
   render() {
     // console.log(this.props)
     return (
-      <div className="login-form-container">
-        <div>
-          {/* snatched from robinhood */}
-          <img src="https://cdn.robinhood.com/assets/generated_assets/1e23d6b90f0d905b425ea289de345ab1.jpg" srcSet="https://cdn.robinhood.com/assets/generated_assets/1e23d6b90f0d905b425ea289de345ab1.jpg 720w, https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg 1440w"/> 
-          {/* srcset="https://cdn.robinhood.com/assets/generated_assets/1e23d6b90f0d905b425ea289de345ab1.jpg 720w, https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg 1440w"/> */}
+      
+      <div className="login-page">
+        <div className="login-space-people">
+          <img src="https://cdn.robinhood.com/assets/generated_assets/1e23d6b90f0d905b425ea289de345ab1.jpg" srcSet="https://cdn.robinhood.com/assets/generated_assets/1e23d6b90f0d905b425ea289de345ab1.jpg 720w, https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg 1440w" height="100%"/> 
         </div>
-        <div>
+        <div className="login-form">          
           <form onSubmit={this.handleSubmit} className="login-form-box">
-                <header>
-                  <span>Welcome to Robinherd</span>
-                </header>
-                        <label>
-                          <span>Username</span>
-                          <input type="text"
-                            value={this.state.username}
-                            onChange={this.update('username')}
-                            className="login-input"
-                          />
-                        </label>
-
-                        <label>
-                          <span>Password</span>
-                          <input type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            className="login-input"
-                          />
-                          <button type="button" ariaLabel="show password in plain text" ariaPressed="false" ariaBusy="false" className="css-755mzn">
-                            <span ariaHidden="true">
-                              <svg fill="none" height="16" role="img" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
-                                <path clipRule="evenodd" d="M1 7.99996C1 7.99996 2.90909 3.54541 8 3.54541C13.0909 3.54541 15 7.99996 15 7.99996C15 7.99996 13.0909 12.4545 8 12.4545C2.90909 12.4545 1 7.99996 1 7.99996ZM5.77254 8.00002C5.77254 9.2282 6.77163 10.2273 7.99982 10.2273C9.228 10.2273 10.2271 9.2282 10.2271 8.00002C10.2271 6.77184 9.228 5.77275 7.99982 5.77275C6.77163 5.77275 5.77254 6.77184 5.77254 8.00002Z" fill="var(--rh__text-color)" fillRule="evenodd" />
-                              </svg>
-                            </span>
-                          </button>
-                        </label>
-                      <footer> 
-                        <div className="login-form">
-                          <button type="submit" className="session-submit">
-                            <span>Sign In</span>
-                          </button>
-                        </div>
-                      </footer>
-            <br/>
-            Please {this.props.formType} or {this.props.navLink}
+            <div><span>Welcome to Robinherd</span></div>
+            <div><span>Username</span></div>
+            <div><input type="text" value={this.state.username} onChange={this.update('username')} className="login-input"/></div>
+            <div><span>Password</span></div>
+            <div><input type="password" value={this.state.password} onChange={this.update('password')} className="login-input"/></div>
+            <div><input type="submit" value="Sign In" className="session-submit"/></div>
+            <div><span>Please {this.props.formType} or {this.props.navLink}</span></div>
             {this.renderErrors()}
           </form>
+        </div>
       </div>
-    </div>
+      
+    
     );
   }
 }
 
 export default SessionForm;
+
+{/* <button type="button" ariaLabel="show password in plain text" ariaPressed="false" ariaBusy="false" className="css-755mzn">
+  <span ariaHidden="true">
+    <svg fill="none" height="16" role="img" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+      <path clipRule="evenodd" d="M1 7.99996C1 7.99996 2.90909 3.54541 8 3.54541C13.0909 3.54541 15 7.99996 15 7.99996C15 7.99996 13.0909 12.4545 8 12.4545C2.90909 12.4545 1 7.99996 1 7.99996ZM5.77254 8.00002C5.77254 9.2282 6.77163 10.2273 7.99982 10.2273C9.228 10.2273 10.2271 9.2282 10.2271 8.00002C10.2271 6.77184 9.228 5.77275 7.99982 5.77275C6.77163 5.77275 5.77254 6.77184 5.77254 8.00002Z" fill="var(--rh__text-color)" fillRule="evenodd" />
+    </svg>
+  </span>
+</button> */}
