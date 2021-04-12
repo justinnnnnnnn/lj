6d4a8e6420b3cd536b23b1e21df2b39656a55049
Justin Cheasty <cheasty@gmail.com>
2021-04-12 00:53:26 -0700

    memefied

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -46,10 +46,11 @@ const Greeting = ({ currentUser, logout }) => {
               preload="auto" 
             >
               
-              <source src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__327bf4cc768a323497d5aaa7416319c2.mp4" type="video/mp4"/>
+              {/* <source src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__327bf4cc768a323497d5aaa7416319c2.mp4" type="video/mp4"/> */}
+              <source src="https://v.redd.it/99yeofbkzms61/DASH_1080.mp4?source=fallback" type="video/mp4"/>
             </video>
 
-              <img class="phone-img" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png" srcSet="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/2x__ff9c36e27d7018cf707b95d8675793a3.png 2x, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__45f00d7b296cb52968f1bca4ef766fc1.png 3x"></img>
+              <img className="phone-img" src="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png" srcSet="https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/1x__284c8d0c799d3c9649ca021c00228275.png, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/2x__ff9c36e27d7018cf707b95d8675793a3.png 2x, https://cdn.robinhood.com/assets/robinhood/brand/_next/static/images/3x__45f00d7b296cb52968f1bca4ef766fc1.png 3x"></img>
           </div>
 
         </div>
@@ -59,7 +60,7 @@ const Greeting = ({ currentUser, logout }) => {
         <span>See our 💎 🙌 </span>
       </div>
 
-      <div className="Errbody">
+      <div className="errbody">
         <div className="superbowl">
           <video 
             autoPlay 
@@ -69,10 +70,23 @@ const Greeting = ({ currentUser, logout }) => {
             playsInline
             preload="auto" 
             >
-            <source src="https://v.redd.it/8w0e23a5jze61/DASH_480.mp4?source=fallback" type="video/mp4"/>
+            <source src="https://v.redd.it/lu8aekujd6e61/DASH_720.mp4?source=fallback" type="video/mp4"/>
+            {/* <source src="https://v.redd.it/8w0e23a5jze61/DASH_480.mp4?source=fallback" type="video/mp4"/> */}
             {/* <source src="https://cdn.robinhood.com/assets/superbowl/superbowl.mp4" type="video/mp4"/> */}
           </video>
         </div>
+        <div className="superbowl-right">
+          <div className="not-cats"><span>We are not cats.</span></div>
+          <div className="not-cat-link">
+            <span><Link to="/signup"><div className="apes-slogan">Apes strong together 🍌</div>
+            {/* <div className="ape-icon"><svg fill="none" height="28" viewBox="0 0 28 28" width="28" xmlns="http://www.w3.org/2000/svg"><path clipRule="evenodd" d="M14 25.6667C20.4433 25.6667 25.6667 20.4434 25.6667 14C25.6667 7.55672 20.4433 2.33337 14 2.33337C7.55668 2.33337 2.33334 7.55672 2.33334 14C2.33334 20.4434 7.55668 25.6667 14 25.6667ZM13.1751 8.99168L17.0168 12.8334H7V15.1667H17.0168L13.1751 19.0084L14.825 20.6583L21.4833 14.0001L14.825 7.34176L13.1751 8.99168Z" fill="black" fillRule="evenodd"></path></svg></div> */}
+            </Link></span>
+          </div>
+        </div>
+      </div>
+
+      <div className="wendy">
+        <h1>Sir, this is a Wendy's</h1>
       </div>
 
     </div>
