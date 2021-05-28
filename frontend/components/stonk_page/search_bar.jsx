ecac751bfde069d85dd239543192915997a41c8c
Justin Cheasty <cheasty@gmail.com>
2021-05-27 17:24:40 -0700

    start search functionality

--- frontend/components/stonk_page/search_bar.jsx
+++ frontend/components/stonk_page/search_bar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+const searchInput = (props) => {
+  // this takes in somethin
+}
+
 const SearchBar = () => {
   return (
     <div className="search-bar">
       <input
         type="text"
         value={"yo mama"}
-        // onChange={}
+        // onChange={searchInput}
         className="search-field"
       />
       {/* thing that inserts all the backend stonks */}
