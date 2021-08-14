dce9893d2f1c9f4d77b19c9a3a86cb0731dfd7ab
justinnnnnnnn <cheasty@gmail.com>
2021-08-13 19:54:02 -0700

    can change shares owned on page via form

--- frontend/components/stonk_page/buy_sell_panel.jsx
+++ frontend/components/stonk_page/buy_sell_panel.jsx
@@ -8,28 +8,57 @@ class BuySellPanel extends React.Component {
     this.state = {
       price: this.props.stonkQuote,
       currentPrice: this.props.stonkQuote.c,
-      input: 0
+      input: 0,
+      sharesOwned: 0,
+      sharesValue: 0
     }
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.submitBuy = this.submitBuy.bind(this);
+    this.setInput = this.setInput.bind(this);
+
+
   }
 
-  setInput = e => this.setState({input: Number(e.target.value)})
+  // handleChange(e) {
+  //   this.setState({value: e.target.value});
+  // }
+
+  setInput(e) {
+    this.setState({input: e.target.value})
+  }
   
+  submitBuy(e) {
+    e.preventDefault();
+    console.log("wtf is e?:", e.target.value)
+    this.setState(
+      {sharesOwned: (Number(this.state.sharesOwned) + Number(this.state.input))}
+    )
+    console.log("shares owned on submit:", this.state.sharesOwned)
+  }
+
   render() {
     const stockPrice = this.state.currentPrice;
     console.log(stockPrice)
-   
 
 
     console.log('quotes:', this.state.price, this.state.currentPrice);
     let buy = () => {
       return (
         <div>
-          <form>
-            Buy {this.props.stonk} <br/>
-            Shares: <input type="number" name="input" value={this.state.input} onChange={this.setInput} placeholder={0} /> <br/>
-            Market Price {`$${stockPrice.toLocaleString('en-US',  {minimumFractionDigits: 2}) }`}<br/>
-            Estimated Cost {`$${(stockPrice * this.state.input).toLocaleString('en-US',  {minimumFractionDigits: 2}) }`}
-          </form>
+          <div>
+            <form onSubmit={this.submitBuy}>
+              Buy {this.props.stonk} <br/>
+              Shares: <input type="number" name="input" value={this.state.input} onChange={this.setInput} placeholder={0} /> <br/>
+              Market Price {`$${stockPrice.toLocaleString('en-US',  {minimumFractionDigits: 2}) }`}<br/>
+              Estimated Cost {`$${(stockPrice * this.state.input).toLocaleString('en-US',  {minimumFractionDigits: 2}) }`}
+              <button>Clickity</button>
+            </form>
+          </div>
+          <div>
+            <br /><br />
+            Shares Owned: {this.state.sharesOwned}
+          </div>
         </div>
       )
     }
