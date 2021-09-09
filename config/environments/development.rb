f451c17a0c05ce2c4899a0e3b055819283c7cc32
justinnnnnnnn <cheasty@gmail.com>
2021-09-08 23:31:30 -0700

    remove prettier deploy heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
-
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
